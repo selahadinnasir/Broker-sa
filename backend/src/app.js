844dfd81951a759abba84239ff628ea71faff7af
selahadinnasir <selahadinas93@gmail.com>
2026-01-06 22:00:00 +0300

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['when deployed', 'http://localhost:5173'],
+    origin: ['https://broker-sa.vercel.app', 'http://localhost:5173'],
     credentials: true,
   })
 );
